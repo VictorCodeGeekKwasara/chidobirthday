fb4dc81577827aeabf2498343c03e483ffb2c230
root <root@DESKTOP-87DPNVK.localdomain>
2022-10-09 21:39:15 +0200

    chido application finish v1

---
+++ scritpt.js
@@ -0,0 +1,29 @@
+const audio = document.querySelector("#music") ;
+const one = document.querySelector(".one") ;
+const two = document.querySelector(".two") ;
+const three = document.querySelector(".three") ;
+const four = document.querySelector(".four") ;
+const text = document.querySelector("#text") ;
+
+
+
+const play = () =>  {
+  one.style.animation = "one 10s linear infinite"
+  // text.style.animation = "text 10s linear infinite"
+  two.style.animation = "two 11s linear infinite"
+  three.style.animation = "three 30s linear infinite"
+  four.style.animation = "four 20s linear infinite" 
+}
+
+const stop = () =>  { 
+  one.style.animation = ""
+  two.style.animation = ""
+  three.style.animation = ""
+  four.style.animation = ""
+}
+
+audio.onplay = () => play()
+audio.onplaying = () => play()
+
+audio.onended = () => stop()
+audio.onpause = () => stop()
